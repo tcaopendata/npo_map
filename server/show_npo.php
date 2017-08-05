ba18932c5fe88be1707e24c39bfe67315b683613
Jackson Chen <teacher144123@gmail.com>
2017-08-05 12:36:17 +0800

    show_npo.php finish

--- server/show_npo.php
+++ server/show_npo.php
@@ -6,9 +6,29 @@
 	//get tag if isset
 	if(isset($_GET['tag'])){
 		$tag = $_GET['tag'];
-		echo $tag;
 	}
 
-	//select the json if isset tag
+	//edit the json if isset tag
+	if(isset($tag)){
+		for($i = 0;$i < count($data['list']);++$i){
+			//echo $i . "~" . $data['list'][$i]['name'] . "<br/>";
+
+			// whether have the tag , 0 is not , 1 is yes
+			$ctr = 0;
+
+			for($j = 0;$j < count($data['list'][$i]['tags']);++$j){
+				//echo "—" . $data['list'][$i]['tags'][$j] . "<br/>";
+				if($data['list'][$i]['tags'][$j] == $tag)$ctr = 1;
+			}
+
+			if($ctr == 0){
+				// don't have tage delete
+				$data['list'][$i]['choose'] = 0;
+			}
+		}
 		echo json_encode($data);
+	}
+	else{
+		echo json_encode($data);
+	}
  ?>
