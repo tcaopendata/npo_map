67ef0a0b970febe2891eac85b9e53d69a4f1f55f
Jackson Chen <teacher144123@gmail.com>
2017-08-06 03:09:15 +0800

    edit show_detail.php add comment

--- server/show_star_avg.php
+++ server/show_star_avg.php
@@ -8,12 +8,14 @@
 		$org_id = $_GET['org_id'];
 		$out = [];
 
-		$sql = 'SELECT COUNT(star) FROM star_record WHERE org_id=:org_id';
+		$sql = 'SELECT SUM(star),COUNT(star) FROM star_record WHERE org_id=:org_id';
 
 		$count_sql = $db->prepare($sql, array(PDO::FETCH_ASSOC));
 	  $count_sql->execute(array(':org_id'=>$org_id));
 	  $row = $count_sql->fetch(PDO::FETCH_ASSOC);
-		echo $row;
+
+		if($row['SUM(star)'] == null)echo "無評分";
+		else echo $row['SUM(star)'] / $row['COUNT(star)'];
 		//echo json_encode($out);
 	}
 	else{
