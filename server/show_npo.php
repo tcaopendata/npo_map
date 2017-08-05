a32051e06a3f5d9e2d21bd8cf70c6e0381beda26
Jackson Chen <teacher144123@gmail.com>
2017-08-05 15:22:33 +0800

    fix show_npo.php

--- server/show_npo.php
+++ server/show_npo.php
@@ -1,34 +1,48 @@
-<?php
+type<?php
 	//get json file
 	$json = file_get_contents("npo_list.json");
 	$data = json_decode($json, true);
 
-	//get tag if isset
-	if(isset($_GET['tag'])){
-		$tag = $_GET['tag'];
+	//get type donate if isset
+	if(isset($_GET['type'])){
+		$type = $_GET['type'];
+	}
+	if(isset($_GET['donate'])){
+		$donate = $_GET['donate'];
 	}
 
-	//edit the json if isset tag
-	if(isset($tag)){
+	//edit the json if isset type
+	if(isset($type)){
 		for($i = 0;$i < count($data['list']);++$i){
-			//echo $i . "~" . $data['list'][$i]['name'] . "<br/>";
-
-			// whether have the tag , 0 is not , 1 is yes
+			// whether have the type , 0 is not , 1 is yes
 			$ctr = 0;
 
-			for($j = 0;$j < count($data['list'][$i]['tags']);++$j){
-				//echo "—" . $data['list'][$i]['tags'][$j] . "<br/>";
-				if($data['list'][$i]['tags'][$j] == $tag)$ctr = 1;
+			for($j = 0;$j < count($data['list'][$i]['type']);++$j){
+				//echo "—" . $data['list'][$i]['type'][$j] . "<br/>";
+				if($data['list'][$i]['type'][$j] == $type)$ctr = 1;
 			}
 
 			if($ctr == 0){
-				// don't have tage delete
+				// don't have typee delete
 				$data['list'][$i]['choose'] = 0;
 			}
 		}
-		echo json_encode($data);
 	}
-	else{
-		echo json_encode($data);
+	if(isset($donate)){
+		for($i = 0;$i < count($data['list']);++$i){
+			// whether have the donate , 0 is not , 1 is yes
+			$ctr = 0;
+
+			for($j = 0;$j < count($data['list'][$i]['donate']);++$j){
+				//echo "—" . $data['list'][$i]['type'][$j] . "<br/>";
+				if($data['list'][$i]['donate'][$j] == $type)$ctr = 1;
+			}
+
+			if($ctr == 0){
+				// don't have typee delete
+				$data['list'][$i]['choose'] = 0;
+			}
+		}
 	}
+	echo json_encode($data);
  ?>
